330f2b1fd7717d10585d54f51737d8a076a892d4
2mas1belino <tomasumbelino03@gmail.com>
2025-07-26 15:31:48 +0100

    feat: add page to register an equipment

--- EquipmentManagement/EquipmentManagement.Shared/Models/Equipment.cs
+++ EquipmentManagement/EquipmentManagement.Shared/Models/Equipment.cs
@@ -1,9 +1,17 @@
-﻿namespace EquipmentManagement.Shared.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EquipmentManagement.Shared.Models;
 
 public class Equipment
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100)]
     public string Model { get; set; } = string.Empty;
     public string? Description { get; set; }
     public byte[]? ImageData { get; set; }
